3ec1c9c53c35a6eb27ed0b3c7ac68efa0c606e38
Th30phaN3 <theophane.pro@tuta.io>
2020-04-15 17:50:14 +0200

    Add theme and level + preferences Co-authored-by: thomas-lefloch <t.lefloch53@gmail.com>

--- database/tables.sql
+++ database/tables.sql
@@ -4,6 +4,8 @@ DROP TABLE IF EXISTS preference;
 DROP TABLE IF EXISTS meeting;
 DROP TABLE IF EXISTS request;
 DROP TABLE IF EXISTS course;
+DROP TABLE IF EXISTS theme;
+DROP TABLE IF EXISTS level;
 DROP TABLE IF EXISTS report;
 DROP TABLE IF EXISTS note;
 DROP TABLE IF EXISTS student;
@@ -25,7 +27,8 @@ CREATE TABLE employee (
     name VARCHAR(200) NOT NULL,
     phone VARCHAR(200) NOT NULL,
     email VARCHAR(200) NOT NULL,
-    id_company UUID NOT NULL REFERENCES company(id)
+    id_company UUID NOT NULL REFERENCES company(id),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE student (
@@ -33,33 +36,47 @@ CREATE TABLE student (
     name VARCHAR(200) NOT NULL,
     phone VARCHAR(200) NOT NULL,
     email VARCHAR(200) NOT NULL,
-    description VARCHAR(200) NOT NULL
+    description VARCHAR(200) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE note (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     value INT NOT NULL,
     id_employee UUID NOT NULL REFERENCES employee(id),
-    id_student UUID NOT NULL REFERENCES student(id)
+    id_student UUID NOT NULL REFERENCES student(id),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE report (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     comment VARCHAR(200) NOT NULL,
-    id_student UUID NOT NULL REFERENCES student(id)
+    id_student UUID NOT NULL REFERENCES student(id),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE theme (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE level (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE course (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    theme VARCHAR(200) NOT NULL,
-    level VARCHAR(200) NOT NULL
+    id_theme UUID NOT NULL REFERENCES theme(id),
+    id_level UUID NOT NULL REFERENCES level(id)
 );
 
 CREATE TABLE request (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     description VARCHAR(200) NOT NULL,
     id_employee UUID NOT NULL REFERENCES employee(id),
-    id_course UUID NOT NULL REFERENCES course(id)
+    id_course UUID NOT NULL REFERENCES course(id),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE meeting (
@@ -78,7 +95,8 @@ CREATE TABLE preference (
 CREATE TABLE file (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     value VARCHAR(200) NOT NULL,
-    id_student UUID NOT NULL REFERENCES student(id)
+    id_student UUID NOT NULL REFERENCES student(id),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE administrator (
@@ -86,24 +104,44 @@ CREATE TABLE administrator (
     name VARCHAR(200) NOT NULL
 );
 
-
-INSERT INTO course
+INSERT INTO theme
 VALUES
-    ('1936cf80-2a8f-4407-94ed-e3f352e6d763', 'Mathématiques', '3ème'),
-    ('05c73d66-f782-4268-80d2-ddfbfbb557c0', 'Mathématiques', '4ème'),
-    ('9ecfc68d-ae58-481c-ae91-a3bfd50cadd9', 'Mathématiques', '5ème'),
-    ('ce1fc079-eb29-4fa6-879b-93d7026a6d17', 'Mathématiques', '6ème'),
-    ('b441074b-ab14-4420-8f13-58ae629e1bb7', 'Mathématiques', 'CM2'),
-    ('9981efc1-6bd1-4f4f-a336-38cc91a3a50a', 'Mathématiques', 'CM1'),
-    ('d172cb78-bac7-4fad-8c15-71992174dc0d', 'Mathématiques', 'CE2'),
-    ('033185ef-7053-48b1-950f-699036831355', 'Mathématiques', 'CE1'),
-    ('4bbb734f-89f1-42c2-b634-8e0a5aed7787', 'Mathématiques', 'CP'),
-    ('c175268c-a084-45c1-b9c0-59428b30506f', 'Français', '3ème'),
-    ('a01e16a7-b2f9-406f-a225-928e61268bd7', 'Français', '4ème'),
-    ('a2276644-e63e-414d-90e7-e099969fd0d4', 'Français', '5ème'),
-    ('c096b602-2866-4ad5-9d46-c0e5ba71fb8e', 'Français', '6ème'),
-    ('3caef256-a1e0-404e-94dd-9c90c9bc5753', 'Français', 'CM2'),
-    ('1a93b1c8-9f0a-4d76-9eb8-3bdab0b68455', 'Français', 'CM1'),
-    ('a28fa928-25b0-4f5c-82f0-5e6d7a1823ec', 'Français', 'CE2'),
-    ('0dbfd640-8424-4412-9ccf-898160944313', 'Français', 'CE1'),
-    ('200ecc19-510c-4e3c-84ad-cf4acac10ca2', 'Français', 'CP');
+    ('6591450e-b404-4d02-b8cf-848fd659c571', 'Mathématiques'),
+    ('ac1d2076-c459-4861-99a3-877f77d7f7a0', 'Français'),
+    ('3356f3a3-f6cc-4ed9-ac5d-37f2654b83e2', 'Physique');
+
+INSERT INTO level
+VALUES
+    ('5d0f381a-91f2-4348-b86d-a7a5adec9e5c', '3ème'),
+    ('eeda5691-01ef-4d73-99fd-cabf32bf38b2', '4ème'),
+    ('511c450a-fae7-4156-a66d-1ab3da0374a3', '5ème'),
+    ('4d6fc3b6-a36e-4484-8200-3cc29024620f', '6ème'),
+    ('010970e5-e4f5-4ba1-bece-d767e985b032', 'CM2'),
+    ('258445c6-f8ee-40e5-a254-2eadb0fb129b', 'CM1'),
+    ('5555219f-a5bc-43ee-8e6a-4ac7ac734008', 'CE2'),
+    ('13cb3cb9-b762-4ebe-8019-ca537675e920', 'CE1'),
+    ('e5423258-d00b-42e4-8a2b-944b46b48067', 'CP');
+
+INSERT INTO course(id_theme, id_level)
+VALUES
+    ('6591450e-b404-4d02-b8cf-848fd659c571', '5d0f381a-91f2-4348-b86d-a7a5adec9e5c'),
+    ('6591450e-b404-4d02-b8cf-848fd659c571', 'eeda5691-01ef-4d73-99fd-cabf32bf38b2'),
+    ('6591450e-b404-4d02-b8cf-848fd659c571', '511c450a-fae7-4156-a66d-1ab3da0374a3'),
+    ('6591450e-b404-4d02-b8cf-848fd659c571', '4d6fc3b6-a36e-4484-8200-3cc29024620f'),
+    ('6591450e-b404-4d02-b8cf-848fd659c571', '010970e5-e4f5-4ba1-bece-d767e985b032'),
+    ('6591450e-b404-4d02-b8cf-848fd659c571', '258445c6-f8ee-40e5-a254-2eadb0fb129b'),
+    ('6591450e-b404-4d02-b8cf-848fd659c571', '5555219f-a5bc-43ee-8e6a-4ac7ac734008'),
+    ('6591450e-b404-4d02-b8cf-848fd659c571', '13cb3cb9-b762-4ebe-8019-ca537675e920'),
+    ('6591450e-b404-4d02-b8cf-848fd659c571', 'e5423258-d00b-42e4-8a2b-944b46b48067'),
+    ('ac1d2076-c459-4861-99a3-877f77d7f7a0', '5d0f381a-91f2-4348-b86d-a7a5adec9e5c'),
+    ('ac1d2076-c459-4861-99a3-877f77d7f7a0', 'eeda5691-01ef-4d73-99fd-cabf32bf38b2'),
+    ('ac1d2076-c459-4861-99a3-877f77d7f7a0', '511c450a-fae7-4156-a66d-1ab3da0374a3'),
+    ('ac1d2076-c459-4861-99a3-877f77d7f7a0', '4d6fc3b6-a36e-4484-8200-3cc29024620f'),
+    ('ac1d2076-c459-4861-99a3-877f77d7f7a0', '010970e5-e4f5-4ba1-bece-d767e985b032'),
+    ('ac1d2076-c459-4861-99a3-877f77d7f7a0', '258445c6-f8ee-40e5-a254-2eadb0fb129b'),
+    ('ac1d2076-c459-4861-99a3-877f77d7f7a0', '5555219f-a5bc-43ee-8e6a-4ac7ac734008'),
+    ('ac1d2076-c459-4861-99a3-877f77d7f7a0', '13cb3cb9-b762-4ebe-8019-ca537675e920'),
+    ('ac1d2076-c459-4861-99a3-877f77d7f7a0', 'e5423258-d00b-42e4-8a2b-944b46b48067'),
+    ('3356f3a3-f6cc-4ed9-ac5d-37f2654b83e2', '5d0f381a-91f2-4348-b86d-a7a5adec9e5c'),
+    ('3356f3a3-f6cc-4ed9-ac5d-37f2654b83e2', 'eeda5691-01ef-4d73-99fd-cabf32bf38b2'),
+    ('3356f3a3-f6cc-4ed9-ac5d-37f2654b83e2', '511c450a-fae7-4156-a66d-1ab3da0374a3');
