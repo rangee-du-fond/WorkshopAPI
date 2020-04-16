73ce9a927f8e04039fb98f070694af33c2ed304c
Th30phaN3 <theophane.pro@tuta.io>
2020-04-16 15:46:16 +0200

    feat: POST and DELETE requests

--- src/requests/requests.controller.ts
+++ src/requests/requests.controller.ts
@@ -9,3 +9,16 @@ export const getRequestById = (requestId: string): Promise<Request> => {
   const stmt = `SELECT * FROM request WHERE request.id = $1`;
   return db.query(stmt, [requestId]).then(res => res.rows[0]);
 };
+
+export const createRequest = (
+  description: string,
+  idEmployee: string,
+  idCourse: string,
+): Promise<string> => {
+  const stmt = `INSERT INTO request(description, id_employee, id_course) VALUES ($1, $2, $3) RETURNING id;`;
+  return db.query(stmt, [description, idEmployee, idCourse]).then(res => res.rows[0].id);
+};
+
+export const deleteRequest = (idRequest: string): Promise<any> => {
+  return db.query(`DELETE FROM request WHERE request.id = $1;`, [idRequest]);
+};
--- src/requests/requests.routes.ts
+++ src/requests/requests.routes.ts
@@ -1,6 +1,8 @@
 import { Server } from '@hapi/hapi';
 import { getAllRequests } from './requests.controller';
 import { getRequestById } from './requests.controller';
+import { createRequest } from './requests.controller';
+import { deleteRequest } from './requests.controller';
 import Joi = require('@hapi/joi');
 
 export const registerRequestsRoutes = (server: Server) => {
@@ -30,6 +32,46 @@ export const registerRequestsRoutes = (server: Server) => {
     },
     handler: (req, h) => {
       return getRequestById(req.params.requestId).catch(console.error);
+    },
+  });
+
+  server.route({
+    method: 'POST',
+    path: '/requests',
+    options: {
+      tags: ['api'],
+      description: 'create a request',
+      validate: {
+        payload: Joi.object({
+          description: Joi.string().required(),
+          idEmployee: Joi.string().required().uuid({ version: 'uuidv4' }),
+          idCourse: Joi.string().required().uuid({ version: 'uuidv4' }),
+        }),
+      },
+    },
+    handler: (req, h) => {
+      return createRequest(
+        (req.payload as any).description,
+        (req.payload as any).idEmployee,
+        (req.payload as any).idCourse,
+      ).catch(console.error);
+    },
+  });
+
+  server.route({
+    method: 'DELETE',
+    path: '/requests/{requestId}',
+    options: {
+      tags: ['api'],
+      description: 'delete a specific request',
+      validate: {
+        params: Joi.object({
+          requestId: Joi.string().required().uuid({ version: 'uuidv4' }),
+        }),
+      },
+    },
+    handler: (req, h) => {
+      return deleteRequest(req.params.requestId).catch(console.error);
     },
   });
 };
