f4b1693029cb37774e0ea721e50e2fa8e3545c0f
Le Floch Thomas <t.lefloch53@gmail.com>
2020-04-18 14:55:08 +0200

    feat: /request/{id} now returns course, employee

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -9,3 +9,13 @@ export const getAllCourses = (): Promise<Course[]> => {
     AND course.id_level=level.id;`;
   return db.query(stmt).then(res => res.rows);
 };
+
+export const findCourseById = (id: string): Promise<Course> => {
+  const stmt = `
+    SELECT course.id, course.id_theme, theme.name as theme_name, course.id_level, level.name as level_name
+    FROM course, theme, level
+    WHERE course.id_theme=theme.id
+    AND course.id_level=level.id
+    AND course.id = $1;`;
+  return db.query(stmt, [id]).then(res => res.rows[0]);
+};
